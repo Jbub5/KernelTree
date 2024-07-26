wuxin9999/kernel_xiaomi_mt6785_HyperOS

drivers/misc: mediatek: guard perfmgr_trace_*
- also guard tracing_record_cmdline
- only use them if we enable tracing

Signed-off-by: Wahid Khan <wahidzk0091@gmail.com>

drivers/misc/mediatek/connectivity/common/connectivity_build_in_adapter.c
@@ -127,12 +127,13 @@ void connectivity_export_show_stack(struct task_struct *tsk, unsigned long *sp)
 }
 EXPORT_SYMBOL(connectivity_export_show_stack);
 
+#ifdef CONFIG_TRACING
 void connectivity_export_tracing_record_cmdline(struct task_struct *tsk)
 {
 	tracing_record_cmdline(tsk);
 }
 EXPORT_SYMBOL(connectivity_export_tracing_record_cmdline);
-
+#endif
 #ifdef CPU_BOOST
 bool connectivity_export_spm_resource_req(unsigned int user,
 					  unsigned int req_mask)

drivers/misc/mediatek/connectivity/common/connectivity_build_in_adapter.h
@@ -187,7 +187,9 @@ void connectivity_export_mt6306_set_gpio_dir(unsigned long pin,
 #endif
 
 #define KERNEL_show_stack connectivity_export_show_stack
+#ifdef CONFIG_TRACING
 #define KERNEL_tracing_record_cmdline connectivity_export_tracing_record_cmdline
+#endif
 #define KERNEL_dump_thread_state connectivity_export_dump_thread_state
 
 #ifdef CPU_BOOST
@@ -205,7 +207,9 @@ void connectivity_export_mt6306_set_gpio_dir(unsigned long pin,
 #define KERNEL_mt_ppm_sysboost_set_freq_limit
 #define KERNEL_spm_resource_req
 #endif
-extern void tracing_record_cmdline(struct task_struct *tsk);
+#ifdef CONFIG_TRACING
+xtern void tracing_record_cmdline(struct task_struct *tsk);
+#endif
 extern void show_stack(struct task_struct *tsk, unsigned long *sp);
 #ifdef CPU_BOOST
 extern void mt_ppm_sysboost_freq(enum ppm_sysboost_user user,
@@ -223,7 +227,9 @@ extern bool spm_resource_req(unsigned int user, unsigned int req_mask);
 
 void connectivity_export_show_stack(struct task_struct *tsk, unsigned long *sp);
 void connectivity_export_dump_thread_state(const char *name);
+#ifdef CONFIG_TRACING
 void connectivity_export_tracing_record_cmdline(struct task_struct *tsk);
+#endif
 #ifdef CPU_BOOST
 void connectivity_export_mt_ppm_sysboost_freq(enum ppm_sysboost_user user,
 					      unsigned int freq);

drivers/misc/mediatek/met_drv/met_api.c
@@ -315,13 +315,13 @@ void met_cpu_frequency(unsigned int frequency, unsigned int cpu_id)
 	trace_cpu_frequency(frequency, cpu_id);
 }
 EXPORT_SYMBOL(met_cpu_frequency);
-
+#ifdef CONFIG_TRACING
 void met_tracing_record_cmdline(struct task_struct *tsk)
 {
 	tracing_record_cmdline(tsk);
 }
 EXPORT_SYMBOL(met_tracing_record_cmdline);
-
+#endif
 void met_set_kptr_restrict(int value)
 {
 	kptr_restrict = value;

drivers/misc/mediatek/performance/include/mtk_perfmgr_internal.h
@@ -67,11 +67,13 @@ extern int check_proc_write(int *data, const char *ubuf, size_t cnt);
 extern int check_group_proc_write(int *cgroup, int *data,
 				 const char *ubuf, size_t cnt);
 
+#ifdef TRACING
 extern void perfmgr_trace_count(int val, const char *fmt, ...);
 extern void perfmgr_trace_end(void);
 extern void perfmgr_trace_begin(char *name, int id, int a, int b);
 extern void perfmgr_trace_printk(char *module, char *string);
 extern void perfmgr_trace_log(char *module, const char *fmt, ...);
+#endif
 extern int perfmgr_common_userlimit_cpu_freq(unsigned int cluster_num,
 					struct ppm_limit_data *final_freq);
 

drivers/misc/mediatek/performance/syslimiter/syslimiter.c
@@ -73,9 +73,9 @@ static void syslimiter_update_limit_freq(void)
 		freq_to_set[cluster_1].max = -1;
 		goto out;
 	}
-
+#ifdef CONFIG_TRACING
 	perfmgr_trace_count(dfrc_fps, "dfrc_fps");
-
+#endif
 	fps60_tolerance = 60 + 60*tolerance_percent/100;
 	fps90_tolerance = 90 + 90*tolerance_percent/100;
 	fps120_tolerance = 120 + 120*tolerance_percent/100;
@@ -84,22 +84,30 @@ static void syslimiter_update_limit_freq(void)
 	if (limit_freq_at_60 > 0 &&
 		dfrc_fps > 0 && dfrc_fps <= fps60_tolerance) {
 		freq_to_set[cluster_1].max = limit_freq_at_60;
+#ifdef CONFIG_TRACING
 		perfmgr_trace_count(limit_freq_at_60, "limit_freq_at_60");
+#endif
 		goto out;
 	} else if (limit_freq_at_90 > 0 &&
 		dfrc_fps > fps60_tolerance && dfrc_fps <= fps90_tolerance) {
 		freq_to_set[cluster_1].max = limit_freq_at_90;
+#ifdef CONFIG_TRACING
 		perfmgr_trace_count(limit_freq_at_90, "limit_freq_at_90");
+#endif
 		goto out;
 	} else if (limit_freq_at_120 > 0 &&
 		dfrc_fps > fps90_tolerance && dfrc_fps <= fps120_tolerance) {
 		freq_to_set[cluster_1].max = limit_freq_at_120;
+#ifdef CONFIG_TRACING
 		perfmgr_trace_count(limit_freq_at_120, "limit_freq_at_120");
+#endif
 		goto out;
 	} else if (limit_freq_at_144 > 0 &&
 		dfrc_fps > fps120_tolerance && dfrc_fps <= fps144_tolerance) {
 		freq_to_set[cluster_1].max = limit_freq_at_144;
+#ifdef CONFIG_TRACING
 		perfmgr_trace_count(limit_freq_at_144, "limit_freq_at_144");
+#endif
 		goto out;
 	} else
 		freq_to_set[cluster_1].max = -1;
@@ -111,8 +119,10 @@ out:
 		update_userlimit_cpu_freq(CPU_KIR_SYSLIM,
 			perfmgr_clusters, freq_to_set);
 		current_freq[cluster_1].max = freq_to_set[cluster_1].max;
+#ifdef CONFIG_TRACING
 		perfmgr_trace_count(current_freq[cluster_1].max,
 			"current_freq");
+#endif
 	}
 	mutex_unlock(&syslimiter);
 

drivers/misc/mediatek/performance/tchbst/user/utch.c
@@ -143,8 +143,9 @@ int notify_touch(int action)
 		WARN_ON(!mutex_is_locked(&notify_lock));
 		isact = is_fstb_active(active_time);
 
+#ifdef CONFIG_TRACING
 		perfmgr_trace_count(isact, "isact");
-
+#endif
 		if ((isact && ktime_to_ms(delta) < time_to_last_touch) ||
 				usrtch_dbg)
 			return ret;
@@ -163,14 +164,18 @@ int notify_touch(int action)
 				perfmgr_clusters, target_freq);
 		if (usrtch_debug)
 			pr_debug("touch down\n");
+#ifdef CONFIG_TRACING
 		perfmgr_trace_count(1, "touch");
+#endif
 		touch_event = 1;
 	} else if (touch_event == 1 && action == 3) {
 		disable_touch_boost_timer();
 		update_eas_uclamp_min(EAS_UCLAMP_KIR_TOUCH, CGROUP_TA, 0);
 		update_userlimit_cpu_freq(CPU_KIR_TOUCH,
 			perfmgr_clusters, reset_freq);
+#ifdef CONFIG_TRACING
 		perfmgr_trace_count(3, "touch");
+#endif
 		touch_event = 2;
 		if (usrtch_debug)
 			pr_debug("touch timeout\n");
@@ -186,7 +191,9 @@ static void notify_touch_up_timeout(struct work_struct *work)
 
 	update_eas_uclamp_min(EAS_UCLAMP_KIR_TOUCH, CGROUP_TA, 0);
 	update_userlimit_cpu_freq(CPU_KIR_TOUCH, perfmgr_clusters, reset_freq);
+#ifdef CONFIG_TRACING
 	perfmgr_trace_count(0, "touch");
+#endif
 	touch_event = 2;
 	if (usrtch_debug)
 		pr_debug("touch timeout\n");

Jbub5/KernelTree

drivers/misc/mediatek/connectivity/common/connectivity_build_in_adapter.c
@@ -127,12 +127,13 @@ void connectivity_export_show_stack(struct task_struct *tsk, unsigned long *sp)
 }
 EXPORT_SYMBOL(connectivity_export_show_stack);
 
+#ifdef DEBUG
 void connectivity_export_tracing_record_cmdline(struct task_struct *tsk)
 {
 	tracing_record_cmdline(tsk);
 }
 EXPORT_SYMBOL(connectivity_export_tracing_record_cmdline);
-
+#endif
 #ifdef CPU_BOOST
 bool connectivity_export_spm_resource_req(unsigned int user,
 					  unsigned int req_mask)

drivers/misc/mediatek/connectivity/common/connectivity_build_in_adapter.h
@@ -187,7 +187,9 @@ void connectivity_export_mt6306_set_gpio_dir(unsigned long pin,
 #endif
 
 #define KERNEL_show_stack connectivity_export_show_stack
+#ifdef DEBUG
 #define KERNEL_tracing_record_cmdline connectivity_export_tracing_record_cmdline
+#endif
 #define KERNEL_dump_thread_state connectivity_export_dump_thread_state
 
 #ifdef CPU_BOOST
@@ -205,7 +207,9 @@ void connectivity_export_mt6306_set_gpio_dir(unsigned long pin,
 #define KERNEL_mt_ppm_sysboost_set_freq_limit
 #define KERNEL_spm_resource_req
 #endif
-extern void tracing_record_cmdline(struct task_struct *tsk);
+#ifdef DEBUG
+xtern void tracing_record_cmdline(struct task_struct *tsk);
+#endif
 extern void show_stack(struct task_struct *tsk, unsigned long *sp);
 #ifdef CPU_BOOST
 extern void mt_ppm_sysboost_freq(enum ppm_sysboost_user user,
@@ -223,7 +227,9 @@ extern bool spm_resource_req(unsigned int user, unsigned int req_mask);
 
 void connectivity_export_show_stack(struct task_struct *tsk, unsigned long *sp);
 void connectivity_export_dump_thread_state(const char *name);
+#ifdef DEBUG
 void connectivity_export_tracing_record_cmdline(struct task_struct *tsk);
+#endif
 #ifdef CPU_BOOST
 void connectivity_export_mt_ppm_sysboost_freq(enum ppm_sysboost_user user,
 					      unsigned int freq);

drivers/misc/mediatek/met_drv/met_api.c
@@ -315,13 +315,13 @@ void met_cpu_frequency(unsigned int frequency, unsigned int cpu_id)
 	trace_cpu_frequency(frequency, cpu_id);
 }
 EXPORT_SYMBOL(met_cpu_frequency);
-
+#ifdef DEBUG
 void met_tracing_record_cmdline(struct task_struct *tsk)
 {
 	tracing_record_cmdline(tsk);
 }
 EXPORT_SYMBOL(met_tracing_record_cmdline);
-
+#endif
 void met_set_kptr_restrict(int value)
 {
 	kptr_restrict = value;

drivers/misc/mediatek/performance/include/mtk_perfmgr_internal.h
@@ -68,11 +68,13 @@ extern int check_proc_write(int *data, const char *ubuf, size_t cnt);
 extern int check_group_proc_write(int *cgroup, int *data,
 				 const char *ubuf, size_t cnt);
 
+#ifdef DEBUG
 extern void perfmgr_trace_count(int val, const char *fmt, ...);
 extern void perfmgr_trace_end(void);
 extern void perfmgr_trace_begin(char *name, int id, int a, int b);
 extern void perfmgr_trace_printk(char *module, char *string);
 extern void perfmgr_trace_log(char *module, const char *fmt, ...);
+#endif
 extern int perfmgr_common_userlimit_cpu_freq(unsigned int cluster_num,
 					struct ppm_limit_data *final_freq);
 

drivers/misc/mediatek/performance/syslimiter/syslimiter.c
@@ -73,9 +73,9 @@ static void syslimiter_update_limit_freq(void)
 		freq_to_set[cluster_1].max = -1;
 		goto out;
 	}
-
+#ifdef DEBUG
 	perfmgr_trace_count(dfrc_fps, "dfrc_fps");
-
+#endif
 	fps60_tolerance = 60 + 60*tolerance_percent/100;
 	fps90_tolerance = 90 + 90*tolerance_percent/100;
 	fps120_tolerance = 120 + 120*tolerance_percent/100;
@@ -84,22 +84,30 @@ static void syslimiter_update_limit_freq(void)
 	if (limit_freq_at_60 > 0 &&
 		dfrc_fps > 0 && dfrc_fps <= fps60_tolerance) {
 		freq_to_set[cluster_1].max = limit_freq_at_60;
+#ifdef DEBUG
 		perfmgr_trace_count(limit_freq_at_60, "limit_freq_at_60");
+#endif
 		goto out;
 	} else if (limit_freq_at_90 > 0 &&
 		dfrc_fps > fps60_tolerance && dfrc_fps <= fps90_tolerance) {
 		freq_to_set[cluster_1].max = limit_freq_at_90;
+#ifdef DEBUG
 		perfmgr_trace_count(limit_freq_at_90, "limit_freq_at_90");
+#endif
 		goto out;
 	} else if (limit_freq_at_120 > 0 &&
 		dfrc_fps > fps90_tolerance && dfrc_fps <= fps120_tolerance) {
 		freq_to_set[cluster_1].max = limit_freq_at_120;
+#ifdef DEBUG
 		perfmgr_trace_count(limit_freq_at_120, "limit_freq_at_120");
+#endif
 		goto out;
 	} else if (limit_freq_at_144 > 0 &&
 		dfrc_fps > fps120_tolerance && dfrc_fps <= fps144_tolerance) {
 		freq_to_set[cluster_1].max = limit_freq_at_144;
+#ifdef DEBUG
 		perfmgr_trace_count(limit_freq_at_144, "limit_freq_at_144");
+#endif
 		goto out;
 	} else
 		freq_to_set[cluster_1].max = -1;
@@ -111,8 +119,10 @@ out:
 		update_userlimit_cpu_freq(CPU_KIR_SYSLIM,
 			perfmgr_clusters, freq_to_set);
 		current_freq[cluster_1].max = freq_to_set[cluster_1].max;
+#ifdef DEBUG
 		perfmgr_trace_count(current_freq[cluster_1].max,
 			"current_freq");
+#endif
 	}
 	mutex_unlock(&syslimiter);
 

drivers/misc/mediatek/performance/tchbst/user/utch.c
@@ -143,8 +143,9 @@ int notify_touch(int action)
 		WARN_ON(!mutex_is_locked(&notify_lock));
 		isact = is_fstb_active(active_time);
 
+#ifdef DEBUG
 		perfmgr_trace_count(isact, "isact");
-
+#endif
 		if ((isact && ktime_to_ms(delta) < time_to_last_touch) ||
 				usrtch_dbg)
 			return ret;
@@ -163,14 +164,18 @@ int notify_touch(int action)
 				perfmgr_clusters, target_freq);
 		if (usrtch_debug)
 			pr_debug("touch down\n");
+#ifdef DEBUG
 		perfmgr_trace_count(1, "touch");
+#endif
 		touch_event = 1;
 	} else if (touch_event == 1 && action == 3) {
 		disable_touch_boost_timer();
 		update_eas_uclamp_min(EAS_UCLAMP_KIR_TOUCH, CGROUP_TA, 0);
 		update_userlimit_cpu_freq(CPU_KIR_TOUCH,
 			perfmgr_clusters, reset_freq);
+#ifdef DEBUG
 		perfmgr_trace_count(3, "touch");
+#endif
 		touch_event = 2;
 		if (usrtch_debug)
 			pr_debug("touch timeout\n");
@@ -186,7 +191,9 @@ static void notify_touch_up_timeout(struct work_struct *work)
 
 	update_eas_uclamp_min(EAS_UCLAMP_KIR_TOUCH, CGROUP_TA, 0);
 	update_userlimit_cpu_freq(CPU_KIR_TOUCH, perfmgr_clusters, reset_freq);
+#ifdef DEBUG
 	perfmgr_trace_count(0, "touch");
+#endif
 	touch_event = 2;
 	if (usrtch_debug)
 		pr_debug("touch timeout\n");