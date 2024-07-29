ab31d45609e917c77bb5544be269619a34cc6526
Jbub5 <133902109+Jbub5@users.noreply.github.com>
2024-07-29 17:46:31 +0300

    Revert "sched/fair: fix misfit with PELT"
    This reverts commit 3dfb009e6ad7afb00eebe96b5bd66beeef135738.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -11978,7 +11978,7 @@ static inline bool nohz_kick_needed(struct rq *rq, bool only_update)
 
 	/* Do idle load balance if there have misfit task */
 	if (energy_aware())
-		return rq->misfit_task_load > 0;
+		return rq->misfit_task_load;
 
 	rcu_read_lock();
 	sds = rcu_dereference(per_cpu(sd_llc_shared, cpu));
